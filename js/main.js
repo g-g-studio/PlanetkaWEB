13b32afc7f4c0ecb04967b6aba8f99f9b3b4cea7
MFoxx <mgally@outlook.com>
2020-02-28 21:37:19 +0100

    Urobenie footeru a scroll animacií

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-import MoveTo from "moveto";
+/*
+  SMOOTH SCROLLING
+*/
+document.querySelectorAll('a[href^="#"]').forEach(anchor => { //Vyberie vsetky hrefy ktore maju # s sebe
+  anchor.addEventListener('click', function (e) { //Prida na nich eventlistener
+      e.preventDefault(); //Zabrani defalut(aby sa to na to hned prepolo)
 
-document.addEventListener('DOMContentLoaded', function(){ 
-  const moveTo = new MoveTo();
-  const target = document.getElementByClassName('sectionII');
-  moveTo.move(target);
+      //Urobi samotny scroll
+      document.querySelector(this.getAttribute('href')).scrollIntoView({ 
+          behavior: 'smooth'
+      });
+  });
 });
+
